f0f1921f8af63b368e0b2d5b0dcf938a6a6fbb40
antrioe <antrioe@gmali.com>
2023-04-14 12:01:18 +0800

    🕹️

--- astro.config.js
+++ astro.config.js
@@ -7,6 +7,10 @@ import node from '@astrojs/node'
 import vercel from '@astrojs/vercel/edge'
 import netlify from '@astrojs/netlify/edge-functions'
 import image from '@astrojs/image'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+import astroI18next from 'astro-i18next'
+import AstroPWA from '@vite-pwa/astro'
 
 function envAdapter() {
   if (process.env.OUTPUT === 'vercel') {
@@ -26,8 +30,14 @@ export default defineConfig({
   integrations: [
     vue(),
     solidJs(),
-    UnoCSS({ /* options */ }),
+    UnoCSS({ injectReset: true }),
     image(),
+    // https://docs.astro.build/en/guides/integrations-guide/sitemap/
+    sitemap(),
+    // https://github.com/alextim/astro-lib/tree/main/packages/astro-robots-txt#readme
+    robotsTxt(),
+    // https://github.com/yassinedoghri/astro-i18next
+    astroI18next(),
     AutoImport({
       /* options */
       include: [
@@ -41,6 +51,45 @@ export default defineConfig({
       dirs: ['src/hooks', 'src/composables', 'src/stores', 'src/utils'],
       dts: 'src/typings/auto-import.d.ts',
       vueTemplate: true,
+    }),
+    // https://vite-pwa-org.netlify.app/frameworks/astro.html
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Astro PWA',
+        short_name: 'Astro PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/404',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
+      },
     }),
   ],
   output: 'server',
